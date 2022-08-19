9fd860a49f2337683095cb684e6980e745d5293a
Zach Banks <zachbanks@icloud.com>
2022-08-19 17:14:22 -0400

    Updated options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,19 +1,46 @@
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.expandtab = true
-vim.opt.undofile = true -- persistant undo file
-vim.opt.numberwidth = 4
-vim.opt.wrap = true -- word wrap
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.cursorline = true
-vim.opt.updatetime = 300 -- faster completiton. default = 400ms
-vim.opt.splitright = true -- forces window splits to right
-vim.opt.splitbelow = true -- forces splits to bottom
-vim.opt.smartindent = true
-vim.opt.showtabline = 2 -- always show tabs
-vim.opt.mouse = "a" -- allow mouse use in all modes
-vim.opt.ignorecase = true -- ignore case in search patterns
-vim.opt.smartcase = false
-vim.opt.clipboard = "unnamedplus" -- allows access to system clipboard
-vim.opt.termguicolors = true
+local options = {
+backup = false,                          -- creates a backup file
+clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+conceallevel = 0,                        -- so that `` is visible in markdown files
+fileencoding = "utf-8",                  -- the encoding written to a file
+hlsearch = true,                         -- highlight all matches on previous search pattern
+ignorecase = true,                       -- ignore case in search patterns
+mouse = "a",                             -- allow the mouse to be used in neovim
+pumheight = 10,                          -- pop up menu height
+showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+showtabline = 2,                         -- always show tabs
+smartcase = true,                        -- smart case
+smartindent = true,                      -- make indenting smarter again
+splitbelow = true,                       -- force all horizontal splits to go below current window
+splitright = true,                       -- force all vertical splits to go to the right of current window
+swapfile = false,                        -- creates a swapfile
+-- termguicolors = true,                    -- set term gui colors (most terminals support this)
+timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+undofile = true,                         -- enable persistent undo
+updatetime = 300,                        -- faster completion (4000ms default)
+writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+expandtab = true,                        -- convert tabs to spaces
+shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+tabstop = 4,                             -- insert 4 spaces for a tab
+cursorline = true,                       -- highlight the current line
+number = true,                           -- set numbered lines
+relativenumber = true,                  -- set relative numbered lines
+numberwidth = 4,                         -- set number column width to 2 {default 4}
+signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+wrap = true,                            -- display lines as one long line
+scrolloff = 8,                           -- is one of my fav
+sidescrolloff = 8,
+guifont = "monospace:h17",               -- the font used in graphical neovim applications
+}
+
+vim.opt.shortmess:append "c"
+
+for k, v in pairs(options) do
+vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
