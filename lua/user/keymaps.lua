5520f6ea74679d4976570faf8c5b53d9a44f41d8
Zach Banks <zachbanks@icloud.com>
2022-08-20 00:08:16 -0400

    Added keymaps for toggle term to help with window nav.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -78,6 +78,17 @@ keymap("n", "<leader>t", ":Telescope live_grep<CR>", opts)
 -- Toggle Term
 keymap('n', '<leader>p', '<cmd>lua _PYTHON_TOGGLE()<CR>', opts) 
 keymap("n", "<leader>g", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", {noremap = true, silent = true})
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
 
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 -- keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 -- keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
